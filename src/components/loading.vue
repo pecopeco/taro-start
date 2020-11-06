9c0dde296bfa33ffae2f9faab0d013d7c50b10f1
pecopeco <yangpeco@qq.com>
2020-11-06 18:39:06 +0800

    update

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -21,69 +21,56 @@ export default {
 }
 </script>
 
-<style vars="{ color, background }">
+<style vars="{ color, background }" lang="stylus">
 
 .loading-wrap {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 300px;
-}
-.loading,
-.loading:before,
-.loading:after {
-  border-radius: 50%;
-}
-.loading {
-  font-size: 11px;
-  text-indent: -99999em;
-  position: relative;
-  width: 10em;
-  height: 10em;
-  color: var(--color);
-  box-shadow: inset 0 0 0 1em;
-  transform: translateZ(0);
-  transform: scale(0.5);
-}
-.loading:before,
-.loading:after {
-  position: absolute;
-  content: '';
-}
-.loading:before {
-  width: 5.2em;
-  height: 10.2em;
-  background: var(--background);
-  border-radius: 10.2em 0 0 10.2em;
-  top: -0.1em;
-  left: -0.1em;
-  transform-origin: 5.1em 5.1em;
-  animation: load 1.5s infinite ease 1.2s;
-}
-.loading:after {
-  width: 5.2em;
-  height: 10.2em;
-  background: var(--background);
-  border-radius: 0 10.2em 10.2em 0;
-  top: -0.1em;
-  left: 4.9em;
-  transform-origin: 0.1em 5.1em;
-  animation: load 1.5s infinite ease;
-}
-@-webkit-keyframes load {
-  0% {
-    transform: rotate(0deg);
+  display flex
+  align-items center
+  justify-content center
+  min-height 300px
+  .loading {
+    position relative
+    width 10em
+    height 10em
+    box-shadow inset 0 0 0 1em
+    border-radius 50%
+    font-size 11px
+    text-indent -99999em
+    color var(--color)
+    transform translateZ(0)
+    transform scale(0.5)
+    &:before {
+      position absolute
+      content ''
+      width 5.2em
+      height 10.2em
+      background var(--background)
+      border-radius 10.2em 0 0 10.2em
+      top -0.1em
+      left -0.1em
+      transform-origin 5.1em 5.1em
+      animation loading 1.5s infinite ease 1.2s
+    }
+    &:after {
+      position absolute
+      content ''
+      width 5.2em
+      height 10.2em
+      background var(--background)
+      border-radius 0 10.2em 10.2em 0
+      top -0.1em
+      left 4.9em
+      transform-origin 0.1em 5.1em
+      animation loading 1.5s infinite ease
+    }
   }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-@keyframes load {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
+  @keyframes loading {
+    0% {
+      transform rotate(0deg)
+    }
+    100% {
+      transform rotate(360deg)
+    }
   }
 }
 </style>
