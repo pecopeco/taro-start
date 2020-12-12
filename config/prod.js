7fabd0fcd9f9979d0771283cdf5ea9dccc1fb41e
pecopeco <yangpeco@qq.com>
2020-12-12 17:06:57 +0800

    update

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,11 @@
+import path from 'path'
+
 module.exports = {
   env: {
     NODE_ENV: '"production"'
+  },
+  alias: {
+    '@': path.resolve(__dirname, '..', 'src')
   },
   defineConstants: {
   },
